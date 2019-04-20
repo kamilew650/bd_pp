a5dd2e09ba3bfa645969efe8b9b21286c29219ed
kamil.lewandowski <kamil.lewandowski1324@gmail.com>
2019-04-20 21:15:39 +0200

    Login

--- PublicTransportClient/public-transport-client/src/app/app.module.ts
+++ PublicTransportClient/public-transport-client/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]

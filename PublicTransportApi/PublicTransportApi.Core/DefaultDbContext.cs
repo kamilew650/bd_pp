467b852faeb6a798a71373007c84f855d93808a9
Dawid Kaczor (dawikac366) <dawikac366@student.polsl.pl>
2019-05-12 19:19:06 +0200

    Dodanie inicjalizacji połączenia z bazą

---
+++ PublicTransportApi/PublicTransportApi.Core/DefaultDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublicTransportApi.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublicTransportApi.Core
+{
+    public class DefaultDbContext : DbContext
+    {
+        public DefaultDbContext(DbContextOptions<DefaultDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Arrival> Arrivals { get; set; }
+        public DbSet<BusStop> BusStops { get; set; }
+        public DbSet<BusStopOnRoute> BusStopsOnRoute { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Failure> Failures { get; set; }
+        public DbSet<Line> Lines { get; set; }
+        public DbSet<Ride> Rides { get; set; }
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<TechnicalReview> TechnicalReviews { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Arrival>().ToTable("Arrival");
+            modelBuilder.Entity<BusStop>().ToTable("BusStop");
+            modelBuilder.Entity<BusStopOnRoute>().ToTable("BusStopOnRoute");
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Failure>().ToTable("Failure");
+            modelBuilder.Entity<Line>().ToTable("Line");
+            modelBuilder.Entity<Ride>().ToTable("Ride");
+            modelBuilder.Entity<Route>().ToTable("Route");
+            modelBuilder.Entity<TechnicalReview>().ToTable("TechnicalReview");
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Vehicle>().ToTable("Vehicle");
+        }
+    }
+}
--- PublicTransportApi/PublicTransportApi/Startup.cs
+++ PublicTransportApi/PublicTransportApi/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using PublicTransportApi.Core;
 
 namespace PublicTransportApi
 {
@@ -26,6 +28,8 @@ namespace PublicTransportApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<DefaultDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

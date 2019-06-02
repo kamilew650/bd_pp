6a70a859ef90764cf5193b5f2aa36fe986d936c3
kamil.lewandowski <kamil.lewandowski1324@gmail.com>
2019-06-02 16:16:19 +0200

    Failure, survey list.

--- PublicTransportClient/public-transport-client/src/app/manager/manager.module.ts
+++ PublicTransportClient/public-transport-client/src/app/manager/manager.module.ts
@@ -4,9 +4,12 @@ import { ManagerComponent } from './manager.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ManagerRoutingModule } from './manager.routing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SurveyComponent } from './survey/survey.component';
+import { FailureComponent } from './failure/failure.component';
+import { VahicleComponent } from './vahicle/vahicle.component';
 
 @NgModule({
-  declarations: [ManagerComponent, NavbarComponent],
+  declarations: [ManagerComponent, NavbarComponent, SurveyComponent, FailureComponent, VahicleComponent],
   imports: [
     CommonModule,
     ManagerRoutingModule,
---
+++ PublicTransportClient/public-transport-client/src/app/manager/vahicle/vahicle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vahicle',
+  templateUrl: './vahicle.component.html',
+  styleUrls: ['./vahicle.component.css']
+})
+export class VahicleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
